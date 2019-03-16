10930d57a2d618f8e8d31c93c1a7900ddfafea1f
David Rachwalik <david.rachwalik@outlook.com>
2019-03-15 23:43:11 -0500

    choco mods

--- ChocolateyScripts/01 Chocolatey Install.ps1
+++ ChocolateyScripts/01 Chocolatey Install.ps1
@@ -1,2 +1,81 @@
-﻿# https://chocolatey.org/packages/launchy
-choco install launchy
+﻿choco install chocolatey
+# https://chocolatey.org/packages/chocolatey-core.extension
+choco install chocolatey-core.extension
+
+
+# ****** General Productivity ******
+# https://chocolatey.org/packages/GoogleChrome
+choco install googlechrome
+# https://chocolatey.org/packages/Office365HomePremium
+choco install office365homepremium
+# https://chocolatey.org/packages/7zip
+choco install 7zip
+# https://chocolatey.org/packages/ccleaner
+choco install ccleaner
+# https://chocolatey.org/packages/defraggler
+# choco install defraggler
+# https://chocolatey.org/packages/Recuva
+# choco install recuva
+# https://chocolatey.org/packages/speccy
+# choco install speccy
+
+
+# ****** Development ******
+# https://chocolatey.org/packages/python3
+# choco install python3
+# https://chocolatey.org/packages/git
+choco install git
+# https://chocolatey.org/packages/dotnetcore-sdk
+choco install dotnetcore-sdk
+# https://chocolatey.org/packages/azure-cli
+choco install azure-cli
+# https://chocolatey.org/packages/microsoftazurestorageexplorer
+choco install microsoftazurestorageexplorer
+# https://chocolatey.org/packages/docker-desktop
+choco install docker-desktop
+
+# https://chocolatey.org/packages/VisualStudio2017Community
+choco install visualstudio2017community
+# https://chocolatey.org/packages/visualstudio2017-workload-netweb
+choco install visualstudio2017-workload-netweb
+# https://chocolatey.org/packages/visualstudio2017-workload-azure
+choco install visualstudio2017-workload-azure
+
+# https://chocolatey.org/packages/vscode
+choco install vscode --params "/NoDesktopIcon /NoContextMenuFiles /NoContextMenuFolders"
+# https://chocolatey.org/packages/vscode-csharp
+choco install vscode-csharp
+# https://chocolatey.org/packages/vscode-icons
+choco install vscode-icons
+
+
+# ****** Media ******
+# https://chocolatey.org/packages/adobereader
+choco install adobereader
+# https://chocolatey.org/packages/AdobeAIR
+choco install adobeair
+# https://chocolatey.org/packages/adobeshockwaveplayer
+choco install adobeshockwaveplayer
+# https://chocolatey.org/packages/geforce-experience
+choco install geforce-experience
+# https://chocolatey.org/packages/gimp
+# choco install gimp
+# https://chocolatey.org/packages/k-litecodecpackmega
+choco install k-litecodecpackmega
+# https://chocolatey.org/packages/obs-studio
+choco install obs-studio
+# https://chocolatey.org/packages/spotify
+choco install spotify
+# https://chocolatey.org/packages/vlc
+# choco install vlc
+
+
+# ****** Videogames ******
+# https://chocolatey.org/packages/autohotkey
+choco install autohotkey
+# https://chocolatey.org/packages/discord
+choco install discord
+# https://chocolatey.org/packages/steam
+choco install steam
+# https://chocolatey.org/packages/twitch
+choco install twitch
--- ChocolateyScripts/02 Chocolatey Upgrade.ps1
+++ ChocolateyScripts/02 Chocolatey Upgrade.ps1
@@ -1,66 +1,47 @@
-﻿choco upgrade chocolatey
+﻿choco upgrade chocolatey-core.extension
 
-# https://chocolatey.org/packages/chocolatey-core.extension
-choco upgrade chocolatey-core.extension
+
+# ****** General Productivity ******
+choco upgrade googlechrome
+choco upgrade office365homepremium
+choco upgrade 7zip
+choco upgrade ccleaner
+# choco upgrade defraggler
+# choco upgrade recuva
+# choco upgrade speccy
 
 
 # ****** Development ******
-# https://chocolatey.org/packages/git
+# choco upgrade python3
 choco upgrade git
-# https://chocolatey.org/packages/dotnetcore-sdk
 choco upgrade dotnetcore-sdk
-# https://chocolatey.org/packages/VisualStudio2017Community
+choco upgrade azure-cli
+choco upgrade microsoftazurestorageexplorer
+choco upgrade docker-desktop
+
 choco upgrade visualstudio2017community
-# https://chocolatey.org/packages/vscode-csharp
-# choco upgrade vscode-csharp
-# https://chocolatey.org/packages/python3
-choco upgrade python3
+choco upgrade visualstudio2017-workload-netweb
+choco upgrade visualstudio2017-workload-azure
+
+choco upgrade vscode
+choco upgrade vscode-csharp
+choco upgrade vscode-icons
 
 
-# https://chocolatey.org/packages/7zip
-choco upgrade 7zip
-
-# https://chocolatey.org/packages/adobereader
+# ****** Media ******
 choco upgrade adobereader
-
-# https://chocolatey.org/packages/AdobeAIR
 choco upgrade adobeair
-
-# https://chocolatey.org/packages/adobeshockwaveplayer
 choco upgrade adobeshockwaveplayer
-
-# https://chocolatey.org/packages/autohotkey
-choco upgrade autohotkey
-
-# https://chocolatey.org/packages/ccleaner
-choco upgrade ccleaner
-
-# https://chocolatey.org/packages/discord
-# choco upgrade discord
-
-# https://chocolatey.org/packages/docker-for-windows
-choco upgrade docker-for-windows
-
-# https://chocolatey.org/packages/gimp
+choco upgrade geforce-experience
 # choco upgrade gimp
-
-# https://chocolatey.org/packages/GoogleChrome
-choco upgrade googlechrome
-
-# https://chocolatey.org/packages/k-litecodecpackmega
 choco upgrade k-litecodecpackmega
-
-# https://chocolatey.org/packages/vlc
+choco upgrade obs-studio
+choco upgrade spotify
 # choco upgrade vlc
 
-# https://chocolatey.org/packages/obs-studio
-choco upgrade obs-studio
 
-# https://chocolatey.org/packages/spotify
-# choco upgrade spotify
-
-# https://chocolatey.org/packages/steam
+# ****** Videogames ******
+choco upgrade autohotkey
+choco upgrade discord
 choco upgrade steam
-
-# https://chocolatey.org/packages/twitch
 choco upgrade twitch
