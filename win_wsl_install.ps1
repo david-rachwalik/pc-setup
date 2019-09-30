3c8006b268c43bfa25ab65daa7a8dec20e05a75d
David Rachwalik <david.rachwalik@outlook.com>
2019-09-29 21:37:29 -0500

    relocate wsl step due to restart timing

--- win_wsl_install.ps1
+++ win_wsl_install.ps1
@@ -1,13 +1,11 @@
 # Run with PowerShell as Administrator
 # https://docs.microsoft.com/en-us/windows/wsl/install-manual
 
-# 1. Install WSL (*nix kernel) and restart system when prompted
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-# 2. Download Ubuntu LTS - the preferred, stable release
+# Download Ubuntu LTS - the preferred, stable release
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile $env:temp\Ubuntu.appx -UseBasicParsing
-# 3. Install Ubuntu
+# Install Ubuntu
 Add-AppxPackage $env:temp\Ubuntu.appx
 
-# 4. Install RemoteRM (leftover commands in D:\Repos\pc-setup\ansible_playbooks\roles\powershell\files\provision_pc.ps1)
+# Install RemoteRM (leftover commands in D:\Repos\pc-setup\ansible_playbooks\roles\powershell\files\provision_pc.ps1)
 
-# 5. Launch Ubuntu (uses WSL)
+# Launch Ubuntu (uses WSL)
