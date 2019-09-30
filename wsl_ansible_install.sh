c1b4cedd2cfe2b02cc82b39c8bcb8423602c48f0
David Rachwalik <david.rachwalik@outlook.com>
2019-09-29 21:40:14 -0500

    relocate wsl update steps

--- wsl_ansible_install.sh
+++ wsl_ansible_install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-# Run command: sudo -H /mnt/d/Repos_Exp/pc-setup/wsl_ansible_install.sh
-
 # Upgrade baseline files of Linux distribution
-# apt-get update && apt full-upgrade -y
+# apt update && apt full-upgrade -y
 apt-get update && apt-get dist-upgrade -y
 
 # Install Ansible dependencies (Python, Git)
@@ -12,26 +10,3 @@ apt-get update && apt-get dist-upgrade -y
 apt-get install -y python-pip git libffi-dev libssl-dev
 # Install Ansible and WinRM
 pip install ansible pywinrm
-
-# Install extra tool to convert Windows files to Linux
-# apt-get install -y dos2unix
-
-
-# mkdir ~/bin
-# Create link in ~/bin to update playbook
-
-
-# https://azure.microsoft.com/en-us/resources/samples/ansible-playbooks/
-# pip install ansible[azure]
-# ansible-galaxy install azure.azure_preview_modules
-# pip install -r ~/.ansible/roles/azure.azure_preview_modules/files/requirements-azure.txt
-
-
-# mkdir /etc/ansible/group_vars
-# mkdir /etc/ansible/host_vars
-# cp /mnt/d/Repos_Exp/pc-setup/etc/ansible/group_vars/windows.yml /etc/ansible/group_vars/windows.yml
-# cp /mnt/d/Repos_Exp/pc-setup/etc/ansible/host_vars/localhost.yml /etc/ansible/host_vars/localhost.yml
-# cp /mnt/d/Repos_Exp/pc-setup/play_test.yml /etc/ansible/play_test.yml
-
-# https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-apt
-# curl -sL https://aka.ms/InstallAzureCLIDeb | bash
