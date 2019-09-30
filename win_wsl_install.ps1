5772c77dccd76dad88c54031d4705bb37c21f219
David Rachwalik <david.rachwalik@outlook.com>
2019-09-29 19:38:43 -0500

    download file to temp directory

--- win_wsl_install.ps1
+++ win_wsl_install.ps1
@@ -4,9 +4,10 @@
 # 1. Install WSL (*nix kernel) and restart system when prompted
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 # 2. Download Ubuntu LTS - the preferred, stable release
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile C:\Users\Public\Ubuntu.appx -UseBasicParsing
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile $env:temp\Ubuntu.appx -UseBasicParsing
 # 3. Install Ubuntu
-Add-AppxPackage C:\Users\Public\Ubuntu.appx
+Add-AppxPackage $env:temp\Ubuntu.appx
+
 # 4. Install RemoteRM (leftover commands in D:\Repos\pc-setup\ansible_playbooks\roles\powershell\files\provision_pc.ps1)
 
 # 5. Launch Ubuntu (uses WSL)
