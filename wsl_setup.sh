e7ccfde54902feb718bfe979646642689c88ea80
David Rachwalik <david.rachwalik@outlook.com>
2019-10-05 15:06:17 -0500

    updates to wsl init

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -11,17 +11,23 @@ apt-get install -y python-pip git libffi-dev libssl-dev
 # Install Ansible and WinRM
 pip install ansible pywinrm
 
+# Create bin directory if it doesn't exist
+if [ ! -d "$HOME/bin" ]; then
+    mkdir $HOME/bin
+fi
+
 
 # --- Git Steps ---
 # https://help.github.com/en/articles/checking-for-existing-ssh-keys
-# ls -al ~/.ssh
-
-# https://help.github.com/en/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -N "" -q
-# start the ssh-agent in the background
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
-
-# https://help.github.com/en/articles/changing-a-remotes-url#switching-remote-urls-from-https-to-ssh
-# Update Git remote from HTTPS to SSH
-git remote set-url origin git@github.com:david-rachwalik/pc-setup.git
+# Generate SSH keys if they don't exist
+if [ ! -d ~/.ssh ]; then
+    # https://help.github.com/en/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+    ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -N "" -q
+    # Start the ssh-agent in the background
+    eval $(ssh-agent -s)
+    ssh-add ~/.ssh/id_rsa
+    
+    # https://help.github.com/en/articles/changing-a-remotes-url#switching-remote-urls-from-https-to-ssh
+    # Update Git remote from HTTPS to SSH
+    # git remote set-url origin git@github.com:david-rachwalik/pc-setup.git
+fi
