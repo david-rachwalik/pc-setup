bc6ce647e63e7af19bdc4aaddaaf05705ca665f1
David Rachwalik <david.rachwalik@outlook.com>
2019-10-06 21:07:19 -0500

    further updates to git config

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -23,8 +23,8 @@ pip install ansible pywinrm
 
 # https://docs.ansible.com/ansible/devel/reference_appendices/config.html#cfg-in-world-writable-dir
 # Ansible will ignore ansible.cfg in a world writable directory (setting directory to user avoids the issue for git clones)
-chown -R david:david ~/pc-setup/
-cd ~/pc-setup/ansible_playbooks/
+chown -R david:david ~/pc-setup
+cd ~/pc-setup/ansible_playbooks
 ansible-playbook wsl_update.yml
 
 
@@ -37,17 +37,28 @@ if [ ! -d ~/.ssh ]; then
     # Start the ssh-agent in the background
     eval $(ssh-agent -s)
     ssh-add ~/.ssh/id_rsa
+    # Add GitHub to known_hosts if it doesn't exist
+    ssh-keygen -F github.com || ssh-keyscan -H github.com >> ~/.ssh/known_hosts
     # Copy the public key string to your GitHub Settings
     # sudo view /root/.ssh/id_rsa.pub
     # view ~/.ssh/id_rsa.pub
 fi
 
+# https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+# View all of your settings
+# git config --list --show-origin
 # Before using Git in VSCode
-git config --global user.email "david.rachwalik@outlook.com"
 git config --global user.name "David Rachwalik"
+git config --global user.email "david.rachwalik@outlook.com"
+# https://git-scm.com/book/en/v2/Appendix-C%3A-Git-Commands-Setup-and-Config#_core_editor
+git config --global core.editor "code --wait"
 
-# git clone https://github.com/david-rachwalik/pc-setup.git ~/pc-setup/
-git clone git@github.com:david-rachwalik/pc-setup.git ~/pc-setup/
+if [ -d ~/pc-setup ]; then
+    rm -rf ~/pc-setup
+fi
+
+# git clone https://github.com/david-rachwalik/pc-setup.git ~/pc-setup
+git clone git@github.com:david-rachwalik/pc-setup.git ~/pc-setup
 # https://help.github.com/en/articles/changing-a-remotes-url#switching-remote-urls-from-https-to-ssh
 # Update Git remote from HTTPS to SSH
 # git remote set-url origin git@github.com:david-rachwalik/pc-setup.git
