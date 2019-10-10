88b4b957639011596a325c4fe3cbec237245895b
David Rachwalik <david.rachwalik@outlook.com>
2019-10-09 22:58:31 -0500

    update to user agnostic

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -2,22 +2,20 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-echo $SUDO_USER
-
 # Upgrade Linux distribution
 apt-get update && apt-get dist-upgrade -y
 # Install Ansible dependencies (Python, Git)
 apt-get install -y python-pip git libffi-dev libssl-dev
 
-if [ ! -f /etc/sudoers.d/david ]; then
-    # Grant user 'david' nopasswd sudo access
+if [ ! -f /etc/sudoers.d/$SUDO_USER ]; then
+    # Grant user nopasswd sudo access
     # https://gist.github.com/carlessanagustin/922711701b1cfcc5c7a056c7018e8fe2
-    touch /etc/sudoers.d/david
-    bash -c "echo '%david ALL=NOPASSWD:ALL' > /etc/sudoers.d/david"
-    chmod 440 /etc/sudoers.d/david
-    usermod -a -G sudo david
+    touch /etc/sudoers.d/$SUDO_USER
+    userGroupCommand = "echo '%" + $SUDO_USER + " ALL=NOPASSWD:ALL' > /etc/sudoers.d/" + $SUDO_USER
+    bash -c $userGroupCommand
+    chmod 440 /etc/sudoers.d/$SUDO_USER
+    usermod -a -G sudo $SUDO_USER
 fi
 
 # Run remaining commands as user
-# sudo -u david curl -s https://raw.githubusercontent.com/david-rachwalik/pc-setup/master/wsl_user_setup.sh | bash
 su -c "curl -s https://raw.githubusercontent.com/david-rachwalik/pc-setup/master/wsl_user_setup.sh | bash" $SUDO_USER
