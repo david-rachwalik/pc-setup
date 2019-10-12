3b8129d5bf9e21c1ec31f75031eecd6201bb480e
David Rachwalik <david.rachwalik@outlook.com>
2019-10-11 23:29:52 -0500

    bash to posix - added sudo check

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 export DEBIAN_FRONTEND=noninteractive
+if test "${SUDO_USER}" != ""; then
+    RUN_USER="${SUDO_USER}"
+else
+    RUN_USER="${USER}"
+    echo "This script must be run with 'sudo'"
+    exit 1
+fi
 
 # Upgrade Linux distribution
 apt-get update && apt-get dist-upgrade -y
@@ -8,14 +15,14 @@ apt-get update && apt-get dist-upgrade -y
 apt-get install -y python-pip git libffi-dev libssl-dev
 apt-get install -y ansible
 
-if [ ! -f /etc/sudoers.d/$SUDO_USER ]; then
+if ! test -f /etc/sudoers.d/${RUN_USER}; then
     # Grant user nopasswd sudo access
     # https://gist.github.com/carlessanagustin/922711701b1cfcc5c7a056c7018e8fe2
-    touch /etc/sudoers.d/$SUDO_USER
-    bash -c "echo '%${SUDO_USER} ALL=NOPASSWD:ALL' > /etc/sudoers.d/${SUDO_USER}"
-    chmod 440 /etc/sudoers.d/$SUDO_USER
-    usermod -a -G sudo $SUDO_USER
+    touch /etc/sudoers.d/${RUN_USER}
+    bash -c "echo '%${RUN_USER} ALL=NOPASSWD:ALL' > /etc/sudoers.d/${RUN_USER}"
+    chmod 440 /etc/sudoers.d/${RUN_USER}
+    usermod -a -G sudo ${RUN_USER}
 fi
 
 # Run remaining commands as user
-# su -c "curl -s https://raw.githubusercontent.com/david-rachwalik/pc-setup/master/wsl_user_setup.sh | bash" $SUDO_USER
+su -c "curl -s https://raw.githubusercontent.com/david-rachwalik/pc-setup/master/wsl_user_setup.sh | bash" ${RUN_USER}
