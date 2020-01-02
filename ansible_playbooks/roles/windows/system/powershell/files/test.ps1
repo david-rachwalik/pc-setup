f8695699d6c48dafe473581568a53c2a1c02fa42
David Rachwalik <david.rachwalik@outlook.com>
2020-01-01 21:22:01 -0600

    powershell role not ready but cleaner

--- ansible_playbooks/roles/windows/system/powershell/files/test.ps1
+++ ansible_playbooks/roles/windows/system/powershell/files/test.ps1
@@ -6,4 +6,4 @@
 # Start Menu: Disable Bing Search Results
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
 # To Restore (Enabled):
-# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 1
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 1
