69575e153b735a461c45b2b32616acc48f20b053
David Rachwalik <david.rachwalik@outlook.com>
2020-01-29 06:38:57 -0600

    updated logging boilerplate

--- ansible_playbooks/roles/python/boilerplate/files/logging_boilerplate.py
+++ ansible_playbooks/roles/python/boilerplate/files/logging_boilerplate.py
@@ -2,56 +2,80 @@
 
 # Basename: logging_boilerplate
 # Description: Common logic to generate log handlers as needed
-# Version: 0.5
-# VersionDate: 13 Dec 2019
+# Version: 1.1
+# VersionDate: 08 Jan 2020
 
 import logging, datetime, pytz
 
 # ------------------------ Classes ------------------------
 
+def IsLogger(logger):
+    return isinstance(logger, logging.Logger)
+
+
+class LoggerOptions(object):
+    def __init__(self, name="", level=logging.WARN, filePath="", logFormat=""):
+        self.name = str(name)
+        # Minimum level of message to log: DEBUG(10)|INFO(20)|WARN(30)|ERROR(40)|FATAL(50)
+        self.level = int(level)
+        self.path = str(filePath)
+        self.format = str(logFormat)
+
+
+# Convert the timezone for log output
+def DisplayTime(*args):
+    utc_date = pytz.utc.localize(datetime.datetime.utcnow())
+    timezone = pytz.timezone("US/Central")
+    converted = utc_date.astimezone(timezone)
+    return converted.timetuple()
+
+
+# Accepts a pre-instantiated logger or LoggerOptions
 class LogManager(object):
-    def __init__(self, logLevel=logging.INFO, logFile=""):
-        # Minimum level of message to log: DEBUG(10)|INFO(20)|WARN|ERROR|FATAL
-        self.logLevel = logLevel
-        # Create formatter to attach to handlers
+    def __init__(self, item=None):
+        self.logger = None
+        # Initialize the logger
+        if IsLogger(item):
+            self.logger = item
+        elif isinstance(item, LoggerOptions):
+            # Obtain instance of logging.Logger based on name (idempotent)
+            self.logger = logging.getLogger(item.name)
+            # Initialize logger output settings and handlers
+            if not self.logger.handlers:
+                self.logger.setLevel(item.level)
+                self.AddHandler(item.level, item,.path)
+                self.logger.debug("(LogManager:__init__) '{0}' logger initialized".format(item.name))
+        else:
+            # Obtain root instance of logging.Logger
+            self.logger = logging.getLogger()
+
+
+    # ciectld-1.1 used logging.basicConfig to create a root logger with default StreamHandler
+    # LogManager generates handlers as needed
+    def AddHandler(self, level=logging.WARN, filePath="")::
+        # Create formatter to attach to handler
         # logMessageFormat = "/%(asctime)s/ [%(levelname)s] %(message)s"
         # logMessageFormat = "[%(levelname)s] %(asctime)s # %(message)s"
         logMessageFormat = "%(asctime)s [%(levelname)s] %(message)s"
         # logTimeFormat = "%Y%b%d %H:%M:%S"
         logTimeFormat = "%Y-%m-%d %H:%M:%S"
-        self.logFormatter = logging.Formatter(fmt=logMessageFormat, datefmt=logTimeFormat)
-        logging.Formatter.converter = self.DisplayTime()
-        # Create logger with file basename (no extension)
-        self.log = logging.getLogger(__name__)
-        self.log.setLevel(self.logLevel)
-        self.AddHandler(logFile)
+        logging.Formatter.converter = DisplayTime
 
-
-    # ciectld-1.1 used logging.basicConfig to create a root logger with default StreamHandler
-    # LogManager generates handlers as needed
-    def AddHandler(self, _logFile=""):
-        if isinstance(logFile, str) and len(logFile) > 0:
+        # TODO: FileHandler minimally tested; improve filePath validation
+        if filePath != "":
             # Setup a file handler for writing to a log file
-            handler = logging.FileHandler(filename=logFile)
+            handler = logging.FileHandler(filename=filePath)
         else:
             # Setup a stream handler for live console output (stdout/stderr)
             handler = logging.StreamHandler()
         # Configure handler with log level and formatter
-        handler.setLevel(self.logLevel)
-        handler.setFormatter(self.logFormatter)
-        self.log.addHandler(handler)
-
-
-    # Convert the timezone for log output
-    def DisplayTime(self, *args):
-        utc_date = pytz.utc.localize(datetime.datetime.utcnow())
-        timezone = pytz.timezone("US/Central")
-        converted = utc_date.astimezone(timezone)
-        return converted.timetuple()
+        handler.setLevel(level)
+        handler.setFormatter(logFormatter)
+        self.logger.addHandler(handler)
 
 
 # ------------------------ Main Program ------------------------
 
 if __name__ == '__main__':
-    boilerplate = ShellManager()
+    boilerplate = LogManager()
     boilerplate.Exit()
