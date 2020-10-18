a7b0e998e5926ef6aa206e76b614eb1b2784e511
David Rachwalik <david.rachwalik@outlook.com>
2020-10-17 22:50:58 -0500

    login working for most common scenarios!

--- ansible_playbooks/roles/linux/system/bash/command/files/az_iterate.sh
+++ ansible_playbooks/roles/linux/system/bash/command/files/az_iterate.sh
@@ -5,5 +5,5 @@
 cd ~/pc-setup/ansible_playbooks
 ansible-playbook system_setup.yml --tags "py" --skip-tags "windows"
 
-# azure --debug login
-azure --debug --secret-key="AutoTestKey" --secret-value="007" secret
+azure --debug login
+# azure --debug --secret-key="AutoTestKey" --secret-value="007" secret
