921fb618384934d4d8f757b7b56f56b1ff7aad0d
David Rachwalik <david.rachwalik@outlook.com>
2022-12-29 22:06:52 -0600

    packages moved near top; added Python path step

--- win_init.ps1
+++ win_init.ps1
@@ -1,55 +1,7 @@
-# Run with PowerShell (as Administrator)
+# -------- Run with PowerShell (as Administrator) --------
 
-# --- Manually ensure PowerShell is updated first (MSI package) ---
-# https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows
-# PowerShell 7.2+ has the option to enable Microsoft Update to handle PowerShell updates
-
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_variables
-# Variable names in PowerShell are NOT case-sensitive and can include spaces and special characters
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators
-
-
-# -------- STAGE 0: Configure PowerShell and OpenSSH --------
-
-# --- Allow execution of PowerShell scripts (.ps1) on machine ---
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies
-# - RemoteSigned policy: allows running scripts created locally but downloaded scripts must be digitally signed by a trusted publisher
-# - Unrestricted policy: carries no restrictions at all and allows unsigned scripts from any source (default for non-Windows)
-$execution_policy = Get-ExecutionPolicy
-$expected_policy = "RemoteSigned"
-if ($execution_policy -ne $expected_policy)
-{
-    Write-Output "PowerShell execution policy is '$execution_policy', changing to '$expected_policy'..."
-    # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy
-    Set-ExecutionPolicy -ExecutionPolicy $expected_policy -Force
-    Write-Output "PowerShell execution policy successfully changed to '$expected_policy'!"
-}
-
-
-# --- Enable Chocolatey to output as PowerShell objects ---
-# TODO: verify whether still necessary
-# $powershell_module_chocolatey = Get-InstalledModule -Name chocolatey
-# if (-not($powershell_module_chocolatey))
-# {
-#     Write-Output "PowerShellGet is missing 'chocolatey' Module, preparing to install..."
-#     # https://learn.microsoft.com/en-us/powershell/module/powershellget/install-module
-#     # https://www.powershellgallery.com/packages/chocolatey/0.0.79
-#     Install-Module -Name chocolatey -RequiredVersion 0.0.79 -Force
-#     Write-Output "PowerShellGet successfully installed 'chocolatey' Module"
-# }
-
-
-# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_server_configuration
-# Set PowerShell as default (instead of Command) for SSH
-# New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
-# Generate public/private keys
-# ssh-keygen -q -f %userprofile%/.ssh/id_rsa -t rsa -b 4096 -N ""
-
-
-# -------- STAGE 1: Install Chocolatey --------
-
-$choco_packages_to_install = @(
-    # https://chocolatey.org/packages/*
+# https://chocolatey.org/packages/*
+$choco_packages = @(
     # --- Productivity ---
     'GoogleChrome'
     '7zip'
@@ -104,6 +56,58 @@ $choco_packages_to_install = @(
 )
 
 
+# ------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+
+
+# -------- STAGE 1: Configure PowerShell and OpenSSH --------
+
+# --- Manually ensure PowerShell is updated first (MSI package) ---
+# https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows
+# PowerShell 7.2+ has the option to enable Microsoft Update to handle PowerShell updates
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_variables
+# Variable names in PowerShell are NOT case-sensitive and can include spaces and special characters
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators
+
+
+# --- Allow execution of PowerShell scripts (.ps1) on machine ---
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies
+# - RemoteSigned policy: allows running scripts created locally but downloaded scripts must be digitally signed by a trusted publisher
+# - Unrestricted policy: carries no restrictions at all and allows unsigned scripts from any source (default for non-Windows)
+$execution_policy = Get-ExecutionPolicy
+$expected_policy = "RemoteSigned"
+if ($execution_policy -ne $expected_policy)
+{
+    Write-Output "PowerShell execution policy is '$execution_policy', changing to '$expected_policy'..."
+    # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy
+    Set-ExecutionPolicy -ExecutionPolicy $expected_policy -Force
+    Write-Output "PowerShell execution policy successfully changed to '$expected_policy'!"
+}
+
+
+# --- Enable Chocolatey to output as PowerShell objects ---
+# TODO: verify whether still necessary
+# $powershell_module_chocolatey = Get-InstalledModule -Name chocolatey
+# if (-not($powershell_module_chocolatey))
+# {
+#     Write-Output "PowerShellGet is missing 'chocolatey' Module, preparing to install..."
+#     # https://learn.microsoft.com/en-us/powershell/module/powershellget/install-module
+#     # https://www.powershellgallery.com/packages/chocolatey/0.0.79
+#     Install-Module -Name chocolatey -RequiredVersion 0.0.79 -Force
+#     Write-Output "PowerShellGet successfully installed 'chocolatey' Module"
+# }
+
+
+# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_server_configuration
+# Set PowerShell as default (instead of Command) for SSH
+# New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+# Generate public/private keys
+# ssh-keygen -q -f %userprofile%/.ssh/id_rsa -t rsa -b 4096 -N ""
+
+
+# -------- STAGE 2: Install Chocolatey --------
+
 # --- Test Chocolatey baseline installation ---
 # $testchoco = Get-Command -Name choco.exe -ErrorAction SilentlyContinue
 $choco_version = choco -v
@@ -127,20 +131,20 @@ else
 
 # --- Verify Chocolatey packages as PowerShell object list ---
 # https://docs.chocolatey.org/en-us/choco/commands/list
-$choco_packages = choco search --local-only --id-only # requires admin
-# $choco_packages_type = $choco_packages.GetType()
-# $choco_packages_length = $choco_packages.Length
+$choco_packages_installed = choco search --local-only --id-only # requires admin
+# $choco_packages_type = $choco_packages_installed.GetType()
+# $choco_packages_length = $choco_packages_installed.Length
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-member
-# $choco_packages_members = $choco_packages | Get-Member
-# $choco_packages_properties = $choco_packages | Get-Member -MemberType Property
-# $choco_packages_methods = $choco_packages | Get-Member -MemberType Method
+# $choco_packages_members = $choco_packages_installed | Get-Member
+# $choco_packages_properties = $choco_packages_installed | Get-Member -MemberType Property
+# $choco_packages_methods = $choco_packages_installed | Get-Member -MemberType Method
 
-# if ($choco_packages)
+# if ($choco_packages_installed)
 # {
 #     Write-Output "results of choco search (truthy)"
 #     Write-Output "---"
 #     Write-Output "choco_packages:"
-#     Write-Output ($choco_packages | Out-String).Trim()
+#     Write-Output ($choco_packages_installed | Out-String).Trim()
 #     Write-Output "---"
 
 #     Write-Output "choco_packages_type: $choco_packages_type"
@@ -148,7 +152,7 @@ $choco_packages = choco search --local-only --id-only # requires admin
 #     Write-Output "choco_packages_properties: $choco_packages_properties"
 
 #     $chrome = "GoogleChrome"
-#     $chrome_found = $choco_packages -contains $chrome
+#     $chrome_found = $choco_packages_installed -contains $chrome
 #     Write-Output "Found $chrome : $chrome_found"
 # }
 # else
@@ -158,11 +162,11 @@ $choco_packages = choco search --local-only --id-only # requires admin
 
 
 # --- Install/Upgrade Chocolatey packages as needed ---
-if ($choco_packages)
+if ($choco_packages_installed)
 {
-    foreach ($package in $choco_packages_to_install | Sort-Object)
+    foreach ($package in $choco_packages | Sort-Object)
     {
-        if (-not ($choco_packages -contains $package))
+        if (-not ($choco_packages_installed -contains $package))
         {
             choco install $package -y
         }
@@ -184,6 +188,40 @@ if ($choco_packages)
 # Start-Process -FilePath "C:\Program Files\Python\python.exe" -ArgumentList '"c:\scripts\simulate.py" "abcd"';
 
 
+# -------- STAGE 3: Configure System Platform --------
+
+# --- Set Environmental Variables ---
+# https://stackoverflow.com/questions/9546324/adding-a-directory-to-the-path-environment-variable-in-windows
+# https://learn.microsoft.com/en-us/powershell/scripting/developer/cmdlet/approved-verbs-for-windows-powershell-commands
+function Add-EnvPath
+{
+    param(
+        [Parameter(Mandatory)]
+        [string]$Dir
+    )
+    
+    if ( !(Test-Path $Dir) )
+    {
+        Write-Warning "Supplied directory was not found!"
+        return
+    }
+    $PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+    if ( $PATH -notlike "*$($Dir)*" )
+    {
+        [Environment]::SetEnvironmentVariable("PATH", "$PATH;$Dir", "Machine")
+    }
+}
+
+$user_py_dir = python -m site --user-base
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/join-path
+$user_py_command_dir = Join-Path -Path $user_py_dir -ChildPath "bin"
+# Write-Output "user_py_command_dir: $user_py_command_dir"
+Add-EnvPath $user_py_command_dir
+
+# https://webinstall.dev/pathman
+# curl.exe https://webi.ms/pathman | powershell
+
+
 Write-Output "--- Windows provisioning has completed ---"
 Exit
 
@@ -192,7 +230,7 @@ Exit
 # -------------------------------------------------------------------------------------
 
 
-# -------- STAGE 2: Install WSL (Windows Subsystem Linux) --------
+# -------- STAGE 4: Install WSL (Windows Subsystem Linux) --------
 
 # https://docs.microsoft.com/en-us/windows/wsl/install-manual
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
@@ -251,7 +289,7 @@ Add-AppxPackage "${wsl_package}"
 # Launch Ubuntu (using WSL)
 
 
-# -------- STAGE 3: Configure System --------
+# -------- STAGE 5: Configure System --------
 
 # --- Provision PowerShell Profile (required for OhMyPosh) ---
 
