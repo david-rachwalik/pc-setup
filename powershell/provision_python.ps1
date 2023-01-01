6bcc61a1c24f2f3b1defb9e7dc85d090bac2512b
David Rachwalik <david.rachwalik@outlook.com>
2023-01-01 14:29:25 -0600

    added function Join-RepoPath for source paths

--- powershell/provision_python.ps1
+++ powershell/provision_python.ps1
@@ -60,137 +60,37 @@ $python_user_commands = @(
 # ------------------------------------------------------------------------------------------------
 
 
-# -------- Provision PIP (Python package-management system) --------
-
-# --- Upgrade PIP installation ---
-Write-Host "Checking for PIP (package manager) upgrades.."
-python -m pip install --upgrade pip
-
-
-# --- Fetch list of installed and out-of-date pip packages ---
-# https://stackoverflow.com/questions/62964466/upgrade-outdated-python-packages-by-pip-in-powershell-in-one-line
-$pip_packages_installed = (pip list --format=json | ConvertFrom-Json).name
-Write-Host "pip_packages_installed: $pip_packages_installed"
-$pip_packages_to_install = $pip_packages | Where-Object { $pip_packages_installed -notcontains $_ }
-Write-Host "pip_packages_to_install: $pip_packages_to_install"
-$pip_packages_outdated = ((pip list --outdated --format=json | ConvertFrom-Json).name | Out-String).replace("`r`n", " ")
-Write-Host "pip_packages_outdated: $pip_packages_outdated"
-
-
-# --- Install missing PIP packages ---
-
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_foreach
-foreach ($package in $pip_packages_to_install)
-{
-    Write-Host "Attempting to install PIP package '$package'.."
-    pip install $package
-}
-
-
-# --- Upgrade all PIP packages ---
-if ($pip_packages_outdated)
-{
-    Write-Host "Attempting to upgrade PIP package '$package'.."
-    pip install --upgrade $pip_packages_outdated
-}
-
-Write-Host "--- Completed provisioning of PIP (package manager) ---"
-
-
-# -------- Provision Python user modules --------
-
-# Write-Host "python_user_modules:"
-# Write-Host ($python_user_modules | Out-String).Trim()
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/out-string
-
-# Write-Host "python_user_boilerplate_modules:"
-# Write-Host ($python_user_boilerplate_modules | Out-String).Trim()
-
-# Write-Host "python_user_commands:"
-# Write-Host ($python_user_commands | Out-String).Trim()
-
-
-# --- Determine Python (source) module locations ---
-
-$script_root = $PSScriptRoot
-[bool]$script_is_remote = 0
-if ($script_root -ne "")
-{
-    # Use local repository when called directly
-    $repo_dir = Split-Path $script_root -Parent
-    $repo_py_dir = Join-Path -Path $repo_dir -ChildPath "python"
-    $repo_py_module_dir = Join-Path -Path $repo_py_dir -ChildPath "modules"
-    $repo_py_boilerplate_dir = Join-Path -Path $repo_py_module_dir -ChildPath "boilerplates"
-    $repo_py_command_dir = Join-Path -Path $repo_py_dir -ChildPath "commands"
-}
-else
-{
-    # Use remote repository when called indirectly (another script)
-    $script_is_remote = 1
-    $repo_url = "https://raw.githubusercontent.com/david-rachwalik/pc-setup/master/"
-    $repo_py_dir = $repo_url + "python"
-    $repo_py_module_dir = $repo_py_dir + "modules"
-    $repo_py_boilerplate_dir = $repo_py_module_dir + "boilerplates"
-    $repo_py_command_dir = $repo_py_dir + "commands"
-}
-Write-Host "source directory: $repo_py_dir"
-
-
-# --- Determine Python (destination) site locations ---
-
-$user_py_dir = python -m site --user-base
-# Write-Host "user_py_dir: $user_py_dir"
-
-$user_py_module_dir = python -m site --user-site
-# Write-Host "user_py_module_dir: $user_py_module_dir"
-# $user_py_boilerplate_dir = Join-Path -Path $user_py_module_dir -ChildPath "boilerplates"
-
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/join-path
-$user_py_command_dir = Join-Path -Path $user_py_dir -ChildPath "bin"
-# Write-Host "user_py_command_dir: $user_py_command_dir"
-
-Write-Host "destination directory: $user_py_dir"
-
-
-# --- Ensure Python (destination) site directories exist ---
-# NO LONGER NECESSARY THANKS TO 'ROBOCOPY'
-# https://stackoverflow.com/questions/16906170/create-directory-if-it-does-not-exist
-# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-path
-# if (!(Test-Path -PathType Container -Path $user_py_module_dir))
-# {
-#     Write-Host "Python user-site directory not found, preparing to create.."
-#     # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item
-#     New-Item -ItemType Directory -Path $user_py_module_dir
-#     Write-Host "Python user-site directory was successfully created!"
-# }
-# # else
-# # {
-# #     Write-Host "Found the Python user-site directory"
-# # }
-
-# if (!(Test-Path -PathType Container -Path $user_py_boilerplate_dir))
-# {
-#     Write-Host "Python user-site boilerplate directory not found, preparing to create.."
-#     # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item
-#     New-Item -ItemType Directory -Path $user_py_boilerplate_dir
-#     Write-Host "Python user-site boilerplate directory was successfully created!"
-# }
-
-# if (!(Test-Path -PathType Container -Path $user_py_command_dir))
-# {
-#     Write-Host "Python command directory not found, preparing to create.."
-#     # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item
-#     New-Item -ItemType Directory -Path $user_py_command_dir
-#     Write-Host "Python command directory was successfully created!"
-# }
-
-
-# --- Method to validate files exist and are identical versions ---
-
 # https://learn.microsoft.com/en-us/powershell/scripting/learn/ps101/09-functions
 # https://learn.microsoft.com/en-us/powershell/scripting/developer/cmdlet/approved-verbs-for-windows-powershell-commands
 
-# Returns true if both files are found and the hashes match
+# Performs a string join on local or remote path
+function Join-RepoPath
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]$RootPath,
+        [Parameter(Mandatory)]
+        [string]$ChildPath,
+        [bool]$IsRemote = 0
+    )
+
+    [string]$Path = ""
+
+    if ($IsRemote)
+    {
+        # Use remote repository when called indirectly (another script)
+        $Path = "$($RootPath)/$($ChildPath)"
+    }
+    else
+    {
+        # Use local repository when called directly
+        # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/join-path
+        $Path = Join-Path -Path $RootPath -ChildPath $ChildPath
+    }
+    return $Path
+}
+
+# Validate files exist and are identical versions
 function Test-FileHashes
 {
     param (
@@ -285,6 +185,131 @@ function Copy-SourceFile
 }
 
 
+# ------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+
+
+# -------- Provision PIP (Python package-management system) --------
+
+# --- Upgrade PIP installation ---
+Write-Host "Checking for PIP (package manager) upgrades.."
+python -m pip install --upgrade pip
+
+
+# --- Fetch list of installed and out-of-date pip packages ---
+# https://stackoverflow.com/questions/62964466/upgrade-outdated-python-packages-by-pip-in-powershell-in-one-line
+$pip_packages_installed = (pip list --format=json | ConvertFrom-Json).name
+Write-Host "pip_packages_installed: $pip_packages_installed"
+$pip_packages_to_install = $pip_packages | Where-Object { $pip_packages_installed -notcontains $_ }
+Write-Host "pip_packages_to_install: $pip_packages_to_install"
+$pip_packages_outdated = ((pip list --outdated --format=json | ConvertFrom-Json).name | Out-String).replace("`r`n", " ")
+Write-Host "pip_packages_outdated: $pip_packages_outdated"
+
+
+# --- Install missing PIP packages ---
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_foreach
+foreach ($package in $pip_packages_to_install)
+{
+    Write-Host "Attempting to install PIP package '$package'.."
+    pip install $package
+}
+
+
+# --- Upgrade all PIP packages ---
+if ($pip_packages_outdated)
+{
+    Write-Host "Attempting to upgrade PIP package '$package'.."
+    pip install --upgrade $pip_packages_outdated
+}
+
+Write-Host "--- Completed provisioning of PIP (package manager) ---" -ForegroundColor Green
+
+
+# -------- Provision Python user modules --------
+
+# Write-Host "python_user_modules:"
+# Write-Host ($python_user_modules | Out-String).Trim()
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/out-string
+
+# Write-Host "python_user_boilerplate_modules:"
+# Write-Host ($python_user_boilerplate_modules | Out-String).Trim()
+
+# Write-Host "python_user_commands:"
+# Write-Host ($python_user_commands | Out-String).Trim()
+
+
+# --- Determine Python (source) module locations ---
+
+$script_root = $PSScriptRoot
+[bool]$script_is_remote = 0
+if ($script_root -ne "")
+{
+    # Use local repository when called directly
+    $repo_dir = Split-Path $script_root -Parent
+}
+else
+{
+    # Use remote repository when called indirectly (another script)
+    $script_is_remote = 1
+    $repo_dir = "https://raw.githubusercontent.com/david-rachwalik/pc-setup/master"
+}
+
+$repo_py_dir = Join-RepoPath $repo_dir "python"
+$repo_py_module_dir = Join-RepoPath $repo_py_dir "modules"
+$repo_py_boilerplate_dir = Join-RepoPath $repo_py_module_dir "boilerplates"
+$repo_py_command_dir = Join-RepoPath $repo_py_dir "commands"
+
+Write-Host "source directory: $repo_py_dir"
+
+
+# --- Determine Python (destination) site locations ---
+
+$user_py_dir = python -m site --user-base
+# Write-Host "user_py_dir: $user_py_dir"
+
+$user_py_module_dir = python -m site --user-site
+# Write-Host "user_py_module_dir: $user_py_module_dir"
+# $user_py_boilerplate_dir = Join-Path -Path $user_py_module_dir -ChildPath "boilerplates"
+
+$user_py_command_dir = Join-Path -Path $user_py_dir -ChildPath "bin"
+# Write-Host "user_py_command_dir: $user_py_command_dir"
+
+Write-Host "destination directory: $user_py_dir"
+
+
+# --- Ensure Python (destination) site directories exist ---
+# NO LONGER NECESSARY THANKS TO 'ROBOCOPY'
+# https://stackoverflow.com/questions/16906170/create-directory-if-it-does-not-exist
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-path
+# if (!(Test-Path -PathType Container -Path $user_py_module_dir))
+# {
+#     Write-Host "Python user-site directory not found, preparing to create.."
+#     # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item
+#     New-Item -ItemType Directory -Path $user_py_module_dir
+#     Write-Host "Python user-site directory was successfully created!"
+# }
+# # else
+# # {
+# #     Write-Host "Found the Python user-site directory"
+# # }
+
+# if (!(Test-Path -PathType Container -Path $user_py_boilerplate_dir))
+# {
+#     Write-Host "Python user-site boilerplate directory not found, preparing to create.."
+#     # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item
+#     New-Item -ItemType Directory -Path $user_py_boilerplate_dir
+#     Write-Host "Python user-site boilerplate directory was successfully created!"
+# }
+
+# if (!(Test-Path -PathType Container -Path $user_py_command_dir))
+# {
+#     Write-Host "Python command directory not found, preparing to create.."
+#     # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item
+#     New-Item -ItemType Directory -Path $user_py_command_dir
+#     Write-Host "Python command directory was successfully created!"
+# }
+
+
 # --- Deploy Python user modules & commands ---
 
 [System.Collections.ArrayList]$PythonFilesPassed = @()
@@ -294,7 +319,7 @@ function Copy-SourceFile
 foreach ($module in $python_user_modules)
 {
     $filename = "$($module).py"
-    $src_path = Join-Path -Path $repo_py_module_dir -ChildPath $filename
+    $src_path = Join-RepoPath $repo_py_module_dir $filename
     $dest_path = Join-Path -Path $user_py_module_dir -ChildPath $filename
     $match = Test-FileHashes $src_path $dest_path $script_is_remote
     if ($match)
@@ -313,7 +338,7 @@ foreach ($module in $python_user_modules)
 foreach ($module in $python_user_boilerplate_modules)
 {
     $filename = "$($module).py"
-    $src_path = Join-Path -Path $repo_py_boilerplate_dir -ChildPath $filename
+    $src_path = Join-RepoPath $repo_py_boilerplate_dir $filename
     $dest_path = Join-Path -Path $user_py_module_dir -ChildPath $filename
     $match = Test-FileHashes $src_path $dest_path $script_is_remote
     if ($match)
@@ -332,7 +357,7 @@ foreach ($module in $python_user_boilerplate_modules)
 foreach ($module in $python_user_commands)
 {
     $filename = "$($module).py"
-    $src_path = Join-Path -Path $repo_py_command_dir -ChildPath $filename
+    $src_path = Join-RepoPath $repo_py_command_dir $filename
     $dest_path = Join-Path -Path $user_py_command_dir -ChildPath $filename
     $match = Test-FileHashes $src_path $dest_path $script_is_remote
     if ($match)
