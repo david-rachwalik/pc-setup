0166fc0042f62ea53ae72485be60013b6dc27c5c
David Rachwalik <david.rachwalik@outlook.com>
2023-01-13 02:42:47 -0600

    scheduled tasks now update; added pc setup cronjob

--- powershell/scheduled_tasks.ps1
+++ powershell/scheduled_tasks.ps1
@@ -23,23 +23,42 @@ function Write-ColorOutput($ForegroundColor)
 
 # -------- Scheduled Tasks (Daily) --------
 
-# --- PC Setup ---
-$taskName = "CRON PC Clean"
+# --- Provision System ---
+$taskName = "CRON PC Setup"
+$action = New-ScheduledTaskAction -Execute "pwsh" -Argument "E:\Repos\pc-setup\powershell\pc_setup.ps1"
+$trigger = New-ScheduledTaskTrigger -Daily -At "3am"
 # $taskExists = Get-ScheduledTask | Where-Object { $_.TaskName -like $taskName }
 $taskExists = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
 if (!$taskExists)
 {
-    Write-Output "Scheduled task '$taskName' does not exist."
-    # $action = New-ScheduledTaskAction -Execute "pwsh" -Argument "E:\Repos\pc-setup\powershell\pc_setup.ps1"
-    $py_exe = "C:\Python311\python.exe"
-    $py_script = "$Env:AppData\Python\bin\pc_clean.py"
-    $action = New-ScheduledTaskAction -Execute $py_exe -Argument $py_script
-    $trigger = New-ScheduledTaskTrigger -Daily -At "3am"
+    Write-Output "Scheduled task '$taskName' does not exist, creating..."
+    # https://learn.microsoft.com/en-us/powershell/module/scheduledtasks/register-scheduledtask
     Register-ScheduledTask -TaskName $taskName -RunLevel "Highest" -Action $action -Trigger $trigger
 }
 else
 {
-    Write-Output "Scheduled task '$taskName' was found."
+    Write-Output "Scheduled task '$taskName' was found, updating..."
+    # https://learn.microsoft.com/en-us/powershell/module/scheduledtasks/set-scheduledtask
+    Set-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger
+}
+
+
+# --- Backup System ---
+$taskName = "CRON PC Clean"
+$py_exe = "C:\Python311\python.exe"
+$py_script = "$Env:AppData\Python\bin\pc_clean.py"
+$action = New-ScheduledTaskAction -Execute $py_exe -Argument $py_script
+$trigger = New-ScheduledTaskTrigger -Daily -At "4am"
+$taskExists = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
+if (!$taskExists)
+{
+    Write-Output "Scheduled task '$taskName' does not exist, creating..."
+    Register-ScheduledTask -TaskName $taskName -RunLevel "Highest" -Action $action -Trigger $trigger
+}
+else
+{
+    Write-Output "Scheduled task '$taskName' was found, updating..."
+    Set-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger
 }
 
 
